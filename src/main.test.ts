abda964dcaf5b50e85a3de02947c746b8c158522
Gabe Johnson <gabe.johnson@gmail.com>
2024-12-13 21:44:53 -0700

    Initial commit

---
+++ src/main.test.ts
@@ -0,0 +1,5 @@
+import { example } from "./main";
+
+test("example", () => {
+  expect(example(4)).toEqual(16);
+});
---
+++ src/main.ts
@@ -0,0 +1,2 @@
+export const example = (input: number) => input * input;
+console.log("Did you know? 4 * 4 = ", example(4));
