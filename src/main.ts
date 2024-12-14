d6024266f481c57863fcb56c2da047e89b7f8ec7
Gabe Johnson <gabe.johnson@gmail.com>
2024-12-14 10:58:46 -0700

    Cleanup, rm unused package

--- src/main.ts
+++ src/main.ts
@@ -3,25 +3,10 @@ import fs from "fs";
 import path from "path";
 import { kebabCase } from "lodash";
 
-const fileName = process.argv[2];
-console.log("file name:", fileName);
-const filePath = path.join(process.cwd(), fileName);
-
-if (!fs.existsSync(filePath)) {
-  console.error(`File not found: ${filePath}`);
-  process.exit(1);
-}
-
 type TestSpec = {
   testName: string;
   points: number;
 };
-const homeworkSpecString = fs.readFileSync(filePath, "utf8");
-const homeworkSpecs: TestSpec[] = JSON.parse(homeworkSpecString);
-console.log(`Found homework file with ${homeworkSpecs.length} entries`);
-
-const doc = load(fs.readFileSync("./template.yml", "utf8"));
-console.log("Loaded yaml template");
 
 type Names = {
   human: string;
@@ -29,6 +14,20 @@ type Names = {
   kebab: string;
   env: string;
 };
+
+// Use commander to parse all this
+const fileName = process.argv[2];
+const filePath = path.join(process.cwd(), fileName);
+
+if (!fs.existsSync(filePath)) {
+  console.error(`File not found: ${filePath}`);
+  process.exit(1);
+}
+
+const homeworkSpecString = fs.readFileSync(filePath, "utf8");
+const homeworkSpecs: TestSpec[] = JSON.parse(homeworkSpecString);
+
+const doc = load(fs.readFileSync("./template.yml", "utf8"));
 
 const toHuman = (functionName: string): string => {
   return kebabCase(functionName) // initTest -> init-test
@@ -87,28 +86,7 @@ const makeEnvObject = (allNames: Names[]): object => {
   );
   return ret;
 };
-/*
-{
-    "name": "Autograding Reporter",
-    "uses": "classroom-resources/autograding-grading-reporter@v1",
-    "env": {
-        "SANITY_RESULTS": "${{steps.sanity.outputs.result}}",
-        "INIT-LIST_RESULTS": "${{steps.init-list.outputs.result}}",
-        "APPEND_RESULTS": "${{steps.append.outputs.result}}",
-        "CONTAINS-VALUE_RESULTS": "${{steps.contains-value.outputs.result}}",
-        "GET-VALUE_RESULTS": "${{steps.get-value.outputs.result}}",
-        "INSERT_RESULTS": "${{steps.insert.outputs.result}}",
-        "REMOVE-AT_RESULTS": "${{steps.remove-at.outputs.result}}",
-        "REMOVE-FIRST_RESULTS": "${{steps.remove-first.outputs.result}}",
-        "REMOVE-LAST_RESULTS": "${{steps.remove-last.outputs.result}}",
-        "REPORT_RESULTS": "${{steps.report.outputs.result}}",
-        "SIZE_RESULTS": "${{steps.size.outputs.result}}"
-    },
-    "with": {
-        "runners": "sanity,init-list,append,contains-value,get-value,insert,remove-at,remove-first,remove-last,report,size"
-    }
-}
-*/
+
 const makeAutogradingReporter = (allSpecs: TestSpec[]) => {
   const allNames = allSpecs.map((spec) => getNames(spec.testName));
   return {
@@ -123,22 +101,13 @@ const makeAutogradingReporter = (allSpecs: TestSpec[]) => {
 
 const steps = homeworkSpecs.map((spec) => makeStep(spec));
 const autogradeStep = makeAutogradingReporter(homeworkSpecs);
-// console.log("autograding step:");
-// console.log(autogradeStep);
-
-// const inYaml = dump(stepAsJson);
-
-// console.log("Template doc as JSON:");
-// console.log(JSON.stringify(doc, null, 4));
-// const originalSteps = doc["jobs"]["run-autograding-tests"]["steps"];
-// const newSteps = [originalSteps[0], makeStep(getNames("initList"), 10)];
 
 doc["jobs"]["run-autograding-tests"]["steps"] = [
   makeCheckoutStep(),
   ...steps,
   autogradeStep,
 ];
-// console.log("new steps:", newSteps);
+
 const inYaml = dump(doc);
-console.log("Final yaml:\n\n");
+
 console.log(inYaml);
